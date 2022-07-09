87ccafb051b4de089fcfb0680a30d9a9fd867c17
Alex_Wright <alexwright976@gmail.com>
2022-07-08 22:02:00 -0400

    psudeocode

---
+++ 04-Web-APIs/02-Challenge/Assets/quiz.js
@@ -0,0 +1,23 @@
+/*
+step 1. display start page- title and papragraph
+and start button
+
+
+Step 2. display question page and hides start page - timer 
+will start when the start button
+is clicked from start page, display question and show 
+4 answer buttons, start page is hidden only show
+question page. when you click one of the answers, 
+show correct or wrong. eaqch question should have 15 seconsds,
+since you have 5 questions. total remaining is 75 for 5 questions.
+when you get a wrong answer your time gets deducted by 15 sec off the clock 
+if you get it right no penalty.
+
+step 3. once you answer all the questions,you will be 
+presented with the score and input your inital and submit button.
+The timer should stop and time left becomes your score
+
+step 4. show a dashboard of all the high scores
+
+
+*/
